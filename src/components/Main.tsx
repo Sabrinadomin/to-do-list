17baca7f19125ad97ba2bdd746c58949f96dcfda
Sabrina Domingues da Conceição <sabrinadomin14@gmail.com>
2023-02-28 11:23:53 -0300

    Added CSS and tasks list.

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,29 +1,53 @@
 import { Component } from 'react';
+import { FaEdit, FaPlus, FaWindowClose } from 'react-icons/fa';
+import './Main.css';
 
 interface State {
   newTask: string
+  tasks: string[]
 }
 export default class Main extends Component<{}, State> {
   state = {
-    newTask: ''
+    newTask: '',
+    tasks: [
+      'Make coffee',
+      'Drink water',
+      'Study'
+    ]
   };
 
   updateState = (e: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ newTask: e.target.value })
+    this.setState({ newTask: e.target.value });
   }
   render() {
-    const { newTask } = this.state
+    const { newTask, tasks } = this.state
 
     return(
       <div className='main'>
         <h1>To-do List</h1>
 
-        <form action='#'>
-          <input onChange= {this.updateState} type='text'/>
-          <button type='submit'>Send</button>
+        <form action='#' className='task-form'>
+          <input
+            onChange= {this.updateState}
+            type='text'
+            value={newTask}
+          />
+          <button type='submit'>
+            <FaPlus/>
+          </button>
         </form>
 
-        <p>{newTask}</p>
+        <ul className='tasks'>
+          {tasks.map(task => (
+            <li key = {task}>
+              {task}
+              <div>
+                <FaEdit className='edit'/>
+                <FaWindowClose className='delete'/>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     );
   }
